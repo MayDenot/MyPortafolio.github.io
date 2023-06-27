32ddd8421827f557a845a980769b862ca3a73681
May <mayra.denot@gmail.com>
2023-06-27 19:08:22 -0300

    Light Mode, legibilidad y contrastes

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,14 @@
-// Variables 
+
 const btnBar = document.getElementById("btn");
 const slideBar = document.getElementById("slidebar");
 
-// Boton toggle de la barra de navegacion
 btnBar.addEventListener('click', () => {
     slideBar.classList.toggle('desplegar');
 })
+
+const blight = document.querySelector('#switch');
+const body = document.querySelector('body');
+
+blight.addEventListener('click', (e) => {
+    body.classList.toggle('lightmode');
+});
